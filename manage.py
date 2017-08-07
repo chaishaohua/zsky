02a00b02edd7bfe14f236e346031a2a818f45435
wenguonideshou <153329152@qq.com>
2017-08-07 08:30:28 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -135,8 +135,8 @@ class Search_Statusreport(db.Model):
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    email = db.Column(db.String(100),nullable=False)
-    name = db.Column(db.String(100),unique=True,nullable=False)
+    email = db.Column(db.String(50),nullable=False)
+    name = db.Column(db.String(50),unique=True,nullable=False)
     password = db.Column(db.String(200),nullable=False)
     def is_authenticated(self):
         return True
